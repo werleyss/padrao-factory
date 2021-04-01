53fcf98fe15f0b0cf630c46a2960c7d316fd52eb
werleyss <werley_desouzasilva@hotmail.com>
2021-04-01 00:44:33 -0300

    Padrão Factory

---
+++ PizzaFactory.php
@@ -0,0 +1,34 @@
+<?php 
+require_once "./ComidaFactory.php";
+require_once "./Pizza.php";
+
+class PizzaFactory implements ComidaFactory{
+    public function criarComida($id)
+    {
+        if($id == 0){
+            $p = new Pizza();
+            $p->nome = "Pizza de calabresa";
+            $p->sabor1 = "Calabresa";
+            $p->sabor2 = "Queijo";
+            $p->comPalmito = false;
+            $p->bordaComRecheio = false;
+            return $p;
+        }elseif($id == 1){
+            $p = new Pizza();
+            $p->nome = "Pizza de Frango";
+            $p->sabor1 = "Frango";
+            $p->sabor2 = "Queijo";
+            $p->comPalmito = true;
+            $p->bordaComRecheio = true;
+            return $p;
+        }else{
+            $p = new Pizza();
+            $p->nome = "Pizza de Misto";
+            $p->sabor1 = "Presuto";
+            $p->sabor2 = "Queijo";
+            $p->comPalmito = true;
+            $p->bordaComRecheio = false;
+            return $p;
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+require_once "./PizzaFactory.php";
+
+$fabrica = new PizzaFactory();
+var_dump($fabrica->criarComida(2));
